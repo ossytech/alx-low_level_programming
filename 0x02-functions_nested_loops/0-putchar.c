7c238c3097df333fbeb0b927ce6a274ba5761e61
Osahon Ibiezugbe <ibiezugbeosahon0@gmail.com>
2023-02-22 04:06:58 -0800

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,13 @@
 
 int main(void)
 {
-    char ch[]= "_putchar";
-
-    putchar(ch);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
     return (0);
 }
