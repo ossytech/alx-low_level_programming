e42afcf514f15b704588180d91ecc4ce8c55e280
ossytech <ibiezugbeosahon@gmail.com>
2023-02-28 12:05:06 -0800

    commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,18 @@
-#include"main.h"
+#include "main.h"
 /**
- * _strlen - returns the lenght of a string
+ * _strlen - returns the length of a string
  * @s: string
  * Return: length
  */
-int _strlen(char *s) 
+int _strlen(char *s)
 {
-    int length = 0;
-    while (*s != '\0') {
-        length++;
-        s++;
-    }
-    return length;
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
 }
